bb826f38360ca591da520c87ce30a2371d81e41e
MehdiBenHamou <94923778+MehdiBenHamou@users.noreply.github.com>
2024-06-16 23:12:23 +0200

    v2

--- 1.4 Script python automatisation/Python-script/hardwareConfig.py
+++ 1.4 Script python automatisation/Python-script/hardwareConfig.py
@@ -1,3 +1,5 @@
+import ipCalculator as ipCalc
+
 # Configuratie van een Cisco switch genereren
 def genereerSwitchConfig(naamLocatie, lokaalNummer, vlanLokalen, subnetLokaal, subnetmaskLokaal, vlanWifi, subnetWifi, subnetmaskWifi):
     config = []
@@ -5,19 +7,23 @@ def genereerSwitchConfig(naamLocatie, lokaalNummer, vlanLokalen, subnetLokaal, s
     # Configuratie van de VLAN voor de lokalen
     vlanId = vlanLokalen + lokaalNummer
     subnetIp = subnetLokaal.split('/')[0]
+    nextSubnetIpDec = ipCalc.dotted2dec(subnetIp) + 1
+    nextSubnetIp = ipCalc.dec2dotted(nextSubnetIpDec)
     config.append(f"vlan {vlanId}")
     config.append(f" name Lokaal_{lokaalNummer + 1}_{naamLocatie}")
     config.append(f"interface vlan {vlanId}")
-    config.append(f" ip address {subnetIp + 1} {subnetmaskLokaal}")
+    config.append(f" ip address {nextSubnetIp} {subnetmaskLokaal}")
     config.append(f" no shutdown")
     config.append("!")
 
     # Configuratie van de VLAN voor de wifi
     wifiIp = subnetWifi.split('/')[0]
+    nextWifiIpDec = ipCalc.dotted2dec(wifiIp) + 1
+    nextWifiIp = ipCalc.dec2dotted(nextWifiIpDec)
     config.append(f"vlan {vlanWifi}")
     config.append(" name WiFi")
     config.append(f"interface vlan {vlanWifi}")
-    config.append(f" ip address {wifiIp + 1} {subnetmaskWifi}")
+    config.append(f" ip address {nextWifiIp} {subnetmaskWifi}")
     config.append(" no shutdown")
     config.append("!")
 
